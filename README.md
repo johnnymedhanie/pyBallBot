6e4a8436d8d90eb7b09a3fba1a7bdc87b18dcb19
johnnymedhanie <jonathanisahyuga@gmail.com>
2017-06-07 20:42:12 -0700

    cleaned up code

--- README.md
+++
@@ -1,2 +0,0 @@
-# pyBallBot
-a python based bot that scrapes data from twitter about basketball and uses data visualization tools to make predictions.
--- twit.py
+++ twit.py
@@ -1,112 +1,67 @@
 import tweepy
-from tweepy import OAuthHandler
-from tweepy import Stream
-from tweepy.streaming import StreamListener #Streams connection
+from tweepy import OAuthHandler, Stream, StreamListener
 import time
 import argparse
 import string
-import config
 import json
-from nltk.tokenize import word_tokenize
-import re
-import operator
-from collections import Counter
-from nltk.corpus import stopwords
+import config
 
-consumer_key = 'LnZwqXojPjni4BMmm6dk3eIRu'
-consumer_secret = 'ERpqOOHp2px4r3VRiqSXP8IZwrkPFlArWQvsAKM9o1x78JSI6A'
-access_token = '827568847-wm5zbFSLUUpYrWwnLwqOznCcOs1wajCFadT8oc1u'
-access_secret = 'O4RWFvomufTxAc7oF3ykR4cg0Q2BEP5YDM3tUb7tQjjIx'
+consumer_key = 'N'
+consumer_secret = 'O'
+access_token = 'P'
+access_secret = 'E'
 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
-api = tweepy.API(auth)
+
+def get_parser():
+    parser = argparse.ArgumentParser(description="NBA twitter stream downloader")
+    parser.add_argument("-q", "--query", dest="query", help="Query/Filter", default='-')
+    parser.add_argument("-d", "--data-dir", dest="data-dir", help="Output/Data Directory")
+    return parser
+
 
 def process_or_store(tweet):
-    with open('data.txt', 'w') as out:
+    with open('data.txt, w') as out:
         json.dumps(tweet, out)
 
-## list of all tweets
-# REMEMBER error code 429 is for too many requests
-for status in tweepy.Cursor(api.home_timeline).items(10):
-    process_or_store(status._json)
-
-""" for friend in tweepy.Cursor(api.friends).items():
-    process_or_store(friend._json) """
-
-""" for tweet in tweepy.Cursor(api.user_timeline).items():
-    process_or_store(tweet._json) """
-
 class MyListener(StreamListener):
-
+    def __init__(self, data_dir, query):
+        query_fname = format_filename
+        self.outfile = "%s/stream_%s".json % (data_dir, query_fname)
     def on_data(self, data):
         try:
-            with open('NBA.json', 'a') as f:
+            with open('base.json', 'a') as f:
                 f.write(data)
                 return True
         except BaseException as e:
-            print("Error on_data: %s" % str(e))
+                print("Error: on_data %s" % str(e))
+                time.sleep(5)
         return True
 
     def on_error(self, status):
-        print(status)
-        return True
+            print(status)
+            return True
+def format_filename(fname):
+    return ''.join(convert_valid(one_char) for one_char in fname)
 
-twitter_stream = Stream(auth, MyListener())
-twitter_stream.filter(track=['#NBA'])
+def convert_valid(one_char):
+    valid_chars = "-_%s%s" % (string.ascii_letters, string.digits)
+    if one_char in valid_chars:
+        return one_char
+    else:
+        return '_'
 
+@classmethod
+def parse(cls, api, raw):
+    status = cls.first_parse(api, raw)
+    setattr(status, 'json', json.dumps(raw))
+    return status
 
+if __name__ = '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+    auth = OAuthHandler(config.consumer_key, config)
+    auth.set_access_token(config.access_token, config.access_secret)
+    api = tweepy.API(auth)
 
-#Test
-# tweet1 = 'RT @johnnymedhanie: test 1 www.google.ca #CODE '
-# print (word_tokenize(tweet1))
-
-emoticons_str = r"""
-(?:
-    [:=;] # these are eyes
-    [oO\-]? # noses
-    [D\)\]\(\]/\\OpP] # mouths ##possible area of going wrong. Check.
-    )"""
-
-regex_str = [
-    emoticons_str,
-    r'<[^>]+>', #HTML tags
-    r'(?:@[\w_]+)', # @-mentions
-    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
-    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
-
-    r'(?:(?:\d+,?)+(?:\.?\d+)?)', ##numbers
-    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
-    r'(?:[\w_]+)', # other words
-    r'(?:\S)' # anything else
-]
-
-token_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
-emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
-
-def tokenize(s):
-    return tokens_re.findall(s)
-
-def preprocess(s, lowercase=False):
-    tokens = tokenize(s)
-    if lowercase:
-        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens ]
-    return tokens
-
-#tryout
-test1 = 'RT @johnnymedhanie: alpha bravo charlie www.johnnymedhanie.github.io #Nice'
-print(preprocess(test1))
-
-
-### COMMON WORDS?
-punctuation - list(string.punctuation)
-stop = stopwords.words('english') + punctuation + ['rt', 'via']
-terms_stop = [term for term in preprocess(tweet['text']) if term not in stop]
-
-fname = 'NBA.json'
-with open(fname, 'a') as f:
-    count_all = Counter()
-    for line in f:
-        tweet = json.loads(line)
-        terms_all = [term for term in preprocess(tweet['text'])]
-        json.dump(count_all.update(terms_all), fname)
+twitter_stream = Stream(auth, MyListener(args.data_dir, args.query))
+twitter_stream.filter(track=[args.query])
