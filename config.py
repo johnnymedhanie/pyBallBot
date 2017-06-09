9ef04e8d61c1029cd32fe5c8b9011ba1ac7245db
johnnymedhanie <jonathanisahyuga@gmail.com>
2017-06-09 14:55:45 -0700

    Getting to clean and count
    Trying to make the tweets more human readible and easier to process, and
    not a huge json file

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = 'LnZwqXojPjni4BMmm6dk3eIRu'
+consumer_secret = 'ERpqOOHp2px4r3VRiqSXP8IZwrkPFlArWQvsAKM9o1x78JSI6A'
+access_token = '827568847-wm5zbFSLUUpYrWwnLwqOznCcOs1wajCFadT8oc1u'
+access_secret = 'O4RWFvomufTxAc7oF3ykR4cg0Q2BEP5YDM3tUb7tQjjIx'
--- twit.py
+++ twit.py
@@ -1,50 +1,71 @@
 import tweepy
-from tweepy import OAuthHandler, Stream, StreamListener
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
 import time
 import argparse
 import string
-import json
 import config
+import json
 
-consumer_key = 'N'
-consumer_secret = 'O'
-access_token = 'P'
-access_secret = 'E'
-
+from nltk.tokenize import word_tokenize
 
 def get_parser():
-    parser = argparse.ArgumentParser(description="NBA twitter stream downloader")
-    parser.add_argument("-q", "--query", dest="query", help="Query/Filter", default='-')
-    parser.add_argument("-d", "--data-dir", dest="data-dir", help="Output/Data Directory")
+    """Get parser for command line arguments."""
+    parser = argparse.ArgumentParser(description="Twitter Downloader")
+    parser.add_argument("-q",
+                        "--query",
+                        dest="query",
+                        help="Query/Filter",
+                        default='-')
+    parser.add_argument("-d",
+                        "--data-dir",
+                        dest="data_dir",
+                        help="Output/Data Directory")
     return parser
 
 
-def process_or_store(tweet):
-    with open('data.txt, w') as out:
-        json.dumps(tweet, out)
-
 class MyListener(StreamListener):
+    """Custom StreamListener for streaming data."""
+
     def __init__(self, data_dir, query):
-        query_fname = format_filename
-        self.outfile = "%s/stream_%s".json % (data_dir, query_fname)
+        query_fname = format_filename(query)
+        self.outfile = "%s/stream_%s.json" % (data_dir, query_fname)
+
     def on_data(self, data):
         try:
-            with open('base.json', 'a') as f:
+            with open(self.outfile, 'a') as f:
                 f.write(data)
+                print(data)
                 return True
         except BaseException as e:
-                print("Error: on_data %s" % str(e))
-                time.sleep(5)
+            print("Error on_data: %s" % str(e))
+            time.sleep(5)
         return True
 
     def on_error(self, status):
-            print(status)
-            return True
+        print(status)
+        return True
+
+
 def format_filename(fname):
+    """Convert file name into a safe string.
+    Arguments:
+        fname -- the file name to convert
+    Return:
+        String -- converted file name
+    """
     return ''.join(convert_valid(one_char) for one_char in fname)
 
+
 def convert_valid(one_char):
-    valid_chars = "-_%s%s" % (string.ascii_letters, string.digits)
+    """Convert a character into '_' if invalid.
+    Arguments:
+        one_char -- the char to convert
+    Return:
+        Character -- converted char
+    """
+    valid_chars = "-_.%s%s" % (string.ascii_letters, string.digits)
     if one_char in valid_chars:
         return one_char
     else:
@@ -56,12 +77,17 @@ def parse(cls, api, raw):
     setattr(status, 'json', json.dumps(raw))
     return status
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     parser = get_parser()
     args = parser.parse_args()
-    auth = OAuthHandler(config.consumer_key, config)
+    auth = OAuthHandler(config.consumer_key, config.consumer_secret)
     auth.set_access_token(config.access_token, config.access_secret)
     api = tweepy.API(auth)
 
-twitter_stream = Stream(auth, MyListener(args.data_dir, args.query))
-twitter_stream.filter(track=[args.query])
+    twitter_stream = Stream(auth, MyListener(args.data_dir, args.query))
+    twitter_stream.filter(track=[args.query])
+
+    # with open('stream_NBA.json', 'r') as f:
+    #     line = f.readline()
+    #     tweet = json.loads(line)
+    #     print(json.dumps(tweet, indent=4))
